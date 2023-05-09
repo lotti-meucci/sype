4aa7ba979350fb5100dc47f0de9aabf8e2c43fa8
Lorenzo Lotti <52128675+LorenzoLotti@users.noreply.github.com>
2023-05-09 23:38:53 +0200

    Revert "Frontend"

--- src/frontend/app/app-routing.module.ts
+++ src/frontend/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GameComponent } from './game/game.component';
-import { RankingsComponent } from './rankings/rankings.component';
-import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [
-  { path: 'play', component: GameComponent },
-  { path: 'rankings', component: RankingsComponent },
-  { path: 'profile', component: ProfileComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/frontend/app/app.component.ts
+++ src/frontend/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +6,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Sype!';
-
-  constructor(private router: Router) { }
-
-  getRoute() {
-    return this.router.url.substring(1);
-  }
+  title = 'sype';
 }

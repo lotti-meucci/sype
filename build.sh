db2d82afb4a2c1e87203402468158cb3cbd457c0
lotti-meucci <114518972+lotti-meucci@users.noreply.github.com>
2023-06-06 10:06:59 +0200

    build.sh chmod fix

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 mkdir docker/server
-chmod -R 777 docker
 cd src/frontend
 npm i
 ng build
 cd ../../
 cp -rf src/frontend/dist/sype/* docker/server
 cp -rf src/api/* docker/server
+chmod -R 777 docker
